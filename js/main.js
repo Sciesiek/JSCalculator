44b800c804a307f7f6dee1b3342bfa0aec42da9f
Sciechu <26565@pwsz-kalisz.pl>
2020-04-14 14:03:26 +0200

    calculator extend fin

--- js/main.js
+++ js/main.js
@@ -1,6 +1,10 @@
 var inputField = document.getElementById('input01');
 
 function input(input){
+    if(dotAlreadyUsedLastTime(input)){
+        return ;
+    }
+
     inputField.value += input;
 }
 
@@ -12,8 +16,56 @@ function clearWholeInput(){
     inputField.value = '';
 }
 
-function calculates(){
-    inputField.value = eval(inputField.value);
+function calculate(){
+    inputField.value = calculateCurrentValue();
+}
+
+function calculatePow(){
+    if(isInputValueCalculable())
+        inputField.value **= 2;
+}
+
+function calculateSquare(){
+    if(isInputValueCalculable())
+        inputField.value = Math.sqrt(inputField.value);
+}
+
+function calculateLn(){
+    if(isInputValueCalculable())
+        inputField.value = Math.log(inputField.value);
+}
+
+function calculateFactorial(){
+    if(isInputValueCalculable())
+        inputField.value = factorial(inputField.value);
+}
+
+function isInputValueCalculable(){
+    calculate();
+    if(isNaN(calculateCurrentValue())){
+        return false;
+    }else{
+        return true;
+    }
+}
+
+function calculateCurrentValue(){
+    try{
+        return eval(inputField.value);
+    }catch(error){
+        return "Wrong input!";
+    }
+}
+
+function factorial(input){
+    if(input > 170){
+        return Infinity;
+    }
+    result = 1;
+    for(let i = 1; i <= input; i++){
+        result *= i; 
+    }
+    return result;
 }
 
 var extented = document.getElementsByClassName('extended')[0];
@@ -24,4 +76,12 @@ function extended(){
 
 function standard(){
     extented.style.display = 'none';
+}
+
+function lastSign(){
+    return inputField.value.slice(-1);
+}
+
+function dotAlreadyUsedLastTime(input){
+    return lastSign() === '.' && input === lastSign();
 }
